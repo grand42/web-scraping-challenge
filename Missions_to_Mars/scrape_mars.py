1e832293c9767e36fbf4aa3a3a5f87ff77015a64
Gretchen A <and01487@umn.edu>
2020-12-19 01:15:37 -0600

    return mars dictionary

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -34,3 +34,58 @@ def scrape():
     base_url = "https://jpl.nasa.gov"
     featured_image_url = base_url + image_url
 
+    ## Mars Facts ##
+
+    # Use pandas to get the tables in the webpage
+    mars_facts_url = "https://space-facts.com/mars/"
+    browser.visit(mars_facts_url)
+    tables = pd.read_html(mars_facts_url)
+    MarsFacts = tables[0]
+    MarsFacts.columns= ["Category", "Value"]
+    MarsFacts_html = MarsFacts.to_html()
+
+    ## Mars Hemispheres ##
+
+    # URL to be scraped for mars hemisphere images
+    hemi_url="https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(hemi_url)
+    html=browser.html
+    hemi_soup = BeautifulSoup(html, "html.parser")
+    # Loop through all the links on the page to obtain images of each available hemispheres
+    hemispheres = hemi_soup.find_all('div', class_ = 'description')
+    hemisphere_images = []
+    for i in range(len(hemispheres)):
+    
+        # Collect Title
+        Title = hemispheres[i].h3.text
+        print(Title)
+        
+        # Collect link
+        SearchLink = hemispheres[i].a['href']
+        
+        # Get high res image
+        high_res_search = "https://astrogeology.usgs.gov" + SearchLink
+    
+        browser.visit(high_res_search)
+        html=browser.html
+        high_res_soup = BeautifulSoup(html, 'html.parser')
+        High_res = high_res_soup.find('div', class_="downloads").find('li').a['href']
+        print(High_res)
+        
+        # Create Dictionary
+        image_dict={}
+        image_dict['Title']=Title
+        image_dict['img_url']=High_res
+        
+        hemisphere_images.append(image_dict)
+
+        ## Mars Dictionary ##
+
+        mars_dict = {}
+        mars_dict = {"news_title":mars_news_title,
+            "news_paragraph":mars_news_p,
+            "featured_image_url":featured_image_url,
+            "fact_table": str(MarsFacts_html)
+            "hemisphere_images":hemisphere_images
+        }
+    return mars_dict
